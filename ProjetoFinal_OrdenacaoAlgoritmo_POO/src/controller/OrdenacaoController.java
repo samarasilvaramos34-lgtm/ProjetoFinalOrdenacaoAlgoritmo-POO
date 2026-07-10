c3309eab0178028ca9ad873de8585400675a3443
Giseli de Oliveira <giselideoliveiraa@gmail.com>
2026-07-09 23:56:09 -0400

    feat: adiciona algoritmos de ordenacao na estrutura correta do projeto

--- ProjetoFinal_OrdenacaoAlgoritmo_POO/src/controller/OrdenacaoController.java
+++ ProjetoFinal_OrdenacaoAlgoritmo_POO/src/controller/OrdenacaoController.java
@@ -1,5 +1,65 @@
 package controller;
 
+import model.Estatisticas;
+
 public class OrdenacaoController {
 
+    public void bubbleSort(int[] vetor, Estatisticas estatisticas) {
+        estatisticas.zerar();
+        int n = vetor.length;
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = 0; j < n - i - 1; j++) {
+                estatisticas.incrementarComparacoes();
+                if(vetor[j] > vetor[j + 1]) {
+                    //Troca
+                    trocar(vetor, j, j + 1);
+                    estatisticas.incrementarTrocas();
+                }
+            }
+        }
+    }
+
+    public void insertionSort(int[] vetor, Estatisticas estatisticas) {
+        estatisticas.zerar();
+        int n = vetor.length;
+        for(int i = 1; i < n; i++) {
+            int chave = vetor[i];
+            int j = i - 1;
+            while(j >= 0) {
+                estatisticas.incrementarComparacoes();
+                if(vetor[j] > chave) {
+                    vetor[j + 1] = vetor[j];
+                    estatisticas.incrementarTrocas();
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            vetor[j + 1] = chave;
+        }
+    }
+
+    public void selectionSort(int[] vetor, Estatisticas estatisticas) {
+        estatisticas.zerar();
+        int n = vetor.length;
+        for(int i = 0; i < n - 1; i++) {
+            int menorIndice = i;
+            for(int j = i + 1; j < n; j++) {
+                estatisticas.incrementarComparacoes();
+                if(vetor[j] < vetor[menorIndice]) {
+                    menorIndice = j;
+                }
+            }
+            if(menorIndice != i) {
+                trocar(vetor, i, menorIndice);
+                estatisticas.incrementarTrocas();
+            }
+        }
+    }
+
+    private void trocar(int[] vetor, int i, int j) {
+        int temp = vetor[i];
+        vetor[i] = vetor[j];
+        vetor[j] = temp;
+    }
 }
